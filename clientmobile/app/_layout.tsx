56f7295830ec075d0f917041ba16e57c267edcf0
Juicestus <jus@gtsbr.org>
2024-12-21 00:42:38 -0600

    Text to speach works in mobile

--- clientmobile/app/_layout.tsx
+++ clientmobile/app/_layout.tsx
@@ -1,6 +1,9 @@
 
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import 'react-native-reanimated';
+import Tts, { Options } from 'react-native-tts';
+
+
 
 const TriButton = ({ text, color }: { text: string, color: string }) => (
   <View style={{ width: '30%', height: 100 }}>
@@ -8,20 +11,24 @@ const TriButton = ({ text, color }: { text: string, color: string }) => (
       title={"\n\n" + text + "\n\n\n"}
       color={color}
       onPress={() => {
-        // Tts.speak('Hello, world!', {
-        //   androidParams: {
-        //     KEY_PARAM_PAN: -1,
-        //     KEY_PARAM_VOLUME: 0.5,
-        //     KEY_PARAM_STREAM: 'STREAM_MUSIC',
-        //   }
-        // } as any);
-        Alert.alert(text);
+        Tts.speak('Pressed ' + text, {
+          androidParams: {
+            KEY_PARAM_PAN: -1,
+            KEY_PARAM_VOLUME: 0.5,
+            KEY_PARAM_STREAM: 'STREAM_MUSIC',
+          }
+        } as Options);
       }}
     />
   </View>
 )
 
 export default function RootLayout() {
+
+Tts.getInitStatus().then(() => {
+  Tts.speak("Bruh");
+  console.log("Init good!");
+});
 
   return (
     <View style={styles.container}>
