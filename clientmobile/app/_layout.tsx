09494183dfaaea0038e693ffad1c891f96343049
Juicestus <jus@gtsbr.org>
2024-12-21 00:28:07 -0600

    Good mobileclient framework for react native

---
+++ clientmobile/app/_layout.tsx
@@ -0,0 +1,62 @@
+
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import 'react-native-reanimated';
+
+const TriButton = ({ text, color }: { text: string, color: string }) => (
+  <View style={{ width: '30%', height: 100 }}>
+    <Button
+      title={"\n\n" + text + "\n\n\n"}
+      color={color}
+      onPress={() => {
+        // Tts.speak('Hello, world!', {
+        //   androidParams: {
+        //     KEY_PARAM_PAN: -1,
+        //     KEY_PARAM_VOLUME: 0.5,
+        //     KEY_PARAM_STREAM: 'STREAM_MUSIC',
+        //   }
+        // } as any);
+        Alert.alert(text);
+      }}
+    />
+  </View>
+)
+
+export default function RootLayout() {
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={styles.inLine}>
+
+          <TriButton text="Cancel" color="red" />
+          <TriButton text="Pause" color="orange" />
+          <TriButton text="Play" color="green" />
+
+        </View>
+      </View>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  inLine: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
